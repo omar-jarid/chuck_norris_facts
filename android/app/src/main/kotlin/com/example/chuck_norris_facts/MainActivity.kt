6a881c821fa697df48bc935938369155deddae2a
Omar Jarid <omarjarid1993@gmail.com>
2025-06-21 19:17:09 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/chuck_norris_facts/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chuck_norris_facts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/omarjarid/chuck_norris_facts/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.omarjarid.chuck_norris_facts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
